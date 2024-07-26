4c0c4e8de2fe3b684863ddca984dceeb7e26ef6d
Dylan Carder <wcarder1@students.kennesaw.edu>
2024-07-25 23:01:38 -0400

    Updated setup for the config and config template

---
+++ js/config.template.js
@@ -0,0 +1,7 @@
+// NOTE: Rename this file to 'config.js' and input your API keys below
+export const OWAPIKey = ''; // OpenWeather API Key
+export const OCAPIKey = ''; // OpenCage API Key
+export const defaultLocation = {
+    "lat": 34.038287, // coordinates of Kennesaw State University
+    "lon": -84.581747
+};
