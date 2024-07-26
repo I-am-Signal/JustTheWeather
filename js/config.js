978c95a694d50aa00da6ed88e2ac5c0265e3e3dc
Dylan Carder <wcarder1@students.kennesaw.edu>
2024-07-25 22:54:57 -0400

    fixed untracked files

--- js/config.js
+++
@@ -1,6 +0,0 @@
-export const OWAPIKey = ''; // OpenWeather API Key
-export const OCAPIKey = ''; // OpenCage API Key
-export const defaultLocation = {
-    "lat": 34.038287, // coordinates of Kennesaw State University
-    "lon": -84.581747
-};
